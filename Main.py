ba40e0b83daf3ab7db7be3fb149d8db82316c1af
Neal <nealbhalodia@gmail.com>
2019-04-24 14:12:45 -0400

    Initial commit

--- Main.py
+++ Main.py
@@ -1,11 +1,21 @@
-def FirstFactorial(num):
+import pymongo
+from pprint import pprint
 
-    if num == 0:
-        return 1
-    else:
-        return num * FirstFactorial(num)
+client = pymongo.MongoClient("mongodb+srv://testuser:987654321@cluster0-ndzg5.mongodb.net/test?retryWrites=true")
+# db = client.admin
 
-# keep this function call here
+print(client.list_database_names())
 
+dblist = client.list_database_names()
+if "DRIVER" in dblist:
+  print("The database exists.")
 
-print FirstFactorial(5)
+mydb = client["myTestDB"]
+mycol = mydb["PhoneNumbers"]
+mydict = ({'Name' : 'Neal', 'Phone Number' : 3019741074}, {'Name' : 'Gopal' , 'Phone Number' : 123456789})
+
+myquery = {'Name' : 'Neal'}
+if mycol.find(myquery):
+    print('This entry has already been added to the collection.')
+else:
+    x = mycol.insert_many(mydict)
